eb6ae8f2d70b63d023b57c39d2ec545a24be11b4
Jeremiah Bassey Asuquo <jbassuquo4@gmail.com>
2020-06-04 16:02:13 +0100

    implemented MY_SCRIPT

--- scripts/jerryasuquo.js
+++ scripts/jerryasuquo.js
@@ -8,7 +8,7 @@ function output() {
     filename: "jerryasuquo.js",
   };
 
-  return `Hello World, this is ${data.firstname} ${data.lastname} with HNGi7 ID ${data.id} using ${data.language} for stage 2 task. ${data.email}`;
+  return `Hello World, this is [${data.firstname}] [${data.lastname}] with HNGi7 ID [${data.id}] using [${data.language}] for stage 2 task. ${data.email}`;
 }
 
 console.log(output());
